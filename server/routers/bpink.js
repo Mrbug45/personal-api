5a8b30016eeebbf4361e09b5e6d44acf63d5ce8f
Me bukan hacker <61094061+Mrbug45@users.noreply.github.com>
2020-12-31 19:57:07 +0700

    Add files via upload

---
+++ server/routers/bpink.js
@@ -0,0 +1,32 @@
+const blackpink require('express').Router();
+const puppeteer = require("puppeteer");
+
+async function getGambar(text) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://textpro.me/create-blackpink-logo-style-online-1001.html');
+
+    await page.type('#text-0', `$(text)`);
+    click page.click('#submit', { delay: 300 });
+
+    await page.waitForSelector('#content-wrapper > section > div > div.col-md-9 > div:nth-child(4) > div > img' {delay: 300});
+    let poster = await page.$eval('#content-wrapper > section > div > div.col-md-9 > div:nth-child(4) > div > img', (element) => {
+        return element.getAttribute("src");
+    });
+    let image = await page.$eval('#content-wrapper > section > div > div.col-md-9 > div.row > div:nth-child(1) > div > div > a', (element) => {
+        return element.getAttribute("href");
+    });
+        browser.close();
+    return { poster, image }
+}
+
+blackpink.get('/', async (req, res) => {
+    var URL = req.query.text;
+    const gets = await getGambar(text);
+    res.json(gets)
+});
+
+module.exports = blackpink;
