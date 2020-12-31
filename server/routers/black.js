19424d0d725780d795e511899c018e23691e6b58
Me bukan hacker <61094061+Mrbug45@users.noreply.github.com>
2020-12-31 21:36:24 +0700

    Update black.js

--- server/routers/black.js
+++ server/routers/black.js
@@ -12,7 +12,7 @@ async function getData(URL) {
     await page.type('#text-0', `$(URL)`);
     await page.click('#submit', { delay: 300 });
 
-    await page.waitForSelector('#content-wrapper > section > div > div.col-md-9 > div:nth-child(4) > div > img' {delay: 300});
+    await page.waitForSelector('#content-wrapper > section > div > div.col-md-9 > div:nth-child(4) > div > img', {delay: 300});
     let images = await page.$eval('#content-wrapper > section > div > div.col-md-9 > div:nth-child(4) > div > img', (element) => {
         return element.getAttribute("src");
     });
