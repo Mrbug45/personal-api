40d1f93a420cec1f7510035398da6785613eac9f
Me bukan hacker <61094061+Mrbug45@users.noreply.github.com>
2021-01-01 07:33:29 +0700

    Add files via upload

---
+++ server/routers/bpink.js
@@ -0,0 +1,29 @@
+const black = require('express').Router();
+const puppeteer = require("puppeteer");
+
+async function getData(URL) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://textpro.me/create-blackpink-logo-style-online-1001.html');
+
+    await page.type('#text-0', `$(URL)`);
+    await page.click('#submit', { delay: 300 });
+    
+    await page.waitForSelector('#content-wrapper > section > div > div.col-md-9 > div.row > div:nth-child(1) > div > div > a', {delay: 300});
+    let images = await page.$eval("#content-wrapper > section > div > div.col-md-9 > div.row > div:nth-child(1) > div > div > a", (element) => {
+        return element.getAttribute("href");
+    });
+        browser.close();
+    return { images }
+
+
+black.get('/', async (req, res) => {
+    var URL = req.query.URL;
+    const gets = await getData(URL);
+    res.json(gets)
+});
+
+module.exports = black;
